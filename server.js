b5a315cba3ffec3dec6e8fa4eea936c161371bea
DaltonHart <daltonvaren@gmail.com>
2018-05-26 14:31:43 -0700

    server init setup

---
+++ server.js
@@ -0,0 +1,34 @@
+// required section
+const express = require("express");
+const app = express();
+
+const controllers = require('./controllers');
+
+
+app.use(express.static('public'));
+
+const db = require(`./models`);
+
+// get controllers
+//app.get('/api', controllers.api.index);
+
+
+//send index to front of server
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+  // res.sendFile('views/index.html' , { root : __dirname});
+})
+
+
+//routes
+//app.get('/api/albums', controllers.albums.index);
+
+
+
+
+// app.use()
+
+// listen
+app.listen(3000, ()=>{
+  console.log("Listening to port 3000");
+})
