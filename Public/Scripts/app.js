67b6ff90fd1588d2b47ed9f8a0dd579026edec47
YOUR_GITHUB_USERNAMEcd <fevillarosa73@gmail.com>
2018-05-30 09:39:13 -0700

    updated location images, some html and css

--- Public/Scripts/app.js
+++ Public/Scripts/app.js
@@ -25,8 +25,20 @@ $(function(){
 });
 
 $("#formButton").click(function(){
-        $("#form1").toggle();
-    });
+        $('form.hidden').toggleClass('showing');
+});
+
+$(function(){
+  $("[type=file]").change(function(){
+  	var file = this.files[0],
+  		reader = new FileReader(),
+  		img = $(this).siblings('img')
+  	reader.onload = function (e) {
+  		img.attr('src', e.target.result);
+  	}
+  	reader.readAsDataURL(file);
+  })
+})
 // $(function(){
 // 		$("[type=file]").change(function(){
 // 			var file = this.files[0],
