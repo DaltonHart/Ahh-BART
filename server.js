f188861beec2936d98df25c3c909b6a5e57e6f68
DaltonHart <daltonvaren@gmail.com>
2018-05-31 18:04:09 -0700

    changed views call

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const db = require(`./models`);
 
 //send index to front of server
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-  // res.sendFile('views/index.html' , { root : __dirname});
+  //res.sendFile(__dirname + '/views/index.html');
+  res.sendFile('Views/index.html' , { root : __dirname});
 })
 
 // get controllers
